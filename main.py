5f82b65fb7268d67207667ae2742b023b3078a22
Garth <gartheherman@gmail.com>
2022-12-27 22:21:38 -0700

    Changing streamlit web interface to main.py

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
-import os
+import functools
+import json
+import requests
+import streamlit as st
 
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def hello_world():
-    name = os.environ.get("NAME", "World")
-    return "Hello {}!".format(name)
+from PIL import Image
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+def generate(img_container=None):
+    text = st.session_state.text_key
+    j_text = json.dumps(text)
+    requests.post(url=f"http://127.0.0.1:8000/prompts/{text}", data=j_text)
+    response = requests.get(url=f"http://127.0.0.1:8000/images/-1")
+    j_response = json.loads(response.content.decode('utf-8'))
+    img_name = j_response['image']
+    img = Image.open(f"images/{img_name}")
+    st.image(img)
+    return img
+
+
+st.text_input(label="Please enter a prompt:", on_change=generate, key='text_key')
+st.button("Regenerate", on_click=generate)
+
+# if prompt:
+#     img_name = generate(text=prompt)
+#     img_name
+#     img = Image.open(f"images/{img_name}")
+#     st.image(img)
